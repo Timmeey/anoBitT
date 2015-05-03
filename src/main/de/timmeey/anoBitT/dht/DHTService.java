9a6c2dbe8a1d964634baa6cff678ac764c158f88
TimmeeY <timmeey@timmeey.de>
2015-05-03 19:33:21 +0200

    JavaDoc for interface Methods

--- src/main/de/timmeey/anoBitT/dht/DHTService.java
+++ src/main/de/timmeey/anoBitT/dht/DHTService.java
@@ -11,8 +11,28 @@ import java.util.List;
  */
 public interface DHTService {
 
+	/**
+	 * Puts a key value Pair into the DHT THis method can be used synchronously
+	 * by setting waitForConfirmation to true
+	 * 
+	 * @param key
+	 *            The key
+	 * @param value
+	 *            The value to be stored in the DHT
+	 * @param waitForConfirmation
+	 *            to set synchronous or asynchronous use
+	 * @return true if asynchronous or the result of the operation if
+	 *         synchronous
+	 */
 	public boolean put(String key, String value, boolean waitForConfirmation);
 
+	/**
+	 * Retrieves a value from the DHT by Key
+	 * 
+	 * @param key
+	 *            The key of the wanted value
+	 * @return the requested value or NULL if not found
+	 */
 	public String get(String key);
 
 	public List<String> getNodes();
