d633bcd87e526834db1fb88478a270bd2afae717
TimmeeY <timmeey@timmeey.de>
2015-06-10 20:16:18 +0200

    Gson instance creator uremoved

--- src/main/de/timmeey/anoBitT/util/GsonSerializer.java
+++ src/main/de/timmeey/anoBitT/util/GsonSerializer.java
@@ -4,23 +4,16 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import java.lang.reflect.Type;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
 
 import com.google.gson.ExclusionStrategy;
 import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.InstanceCreator;
 
 import de.timmeey.libTimmeey.networking.NetSerializer;
 
 public class GsonSerializer implements NetSerializer {
-	Gson gson = new GsonBuilder().registerTypeAdapter(PublicKey.class,
-			new PublicKeyInstanceCreator()).create();
+	Gson gson = new GsonBuilder().create();
 
 	@Override
 	public String toJson(Object object) {
@@ -33,40 +26,21 @@ public class GsonSerializer implements NetSerializer {
 	}
 
 }
-
-class AnnotationExclusionStrategy implements ExclusionStrategy {
-
-	@Override
-	public boolean shouldSkipField(FieldAttributes f) {
-		return f.getAnnotation(Exclude.class) != null;
-	}
-
-	@Override
-	public boolean shouldSkipClass(Class<?> clazz) {
-		return false;
-	}
-}
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-@interface Exclude {
-}
-
-class PublicKeyInstanceCreator implements
-		InstanceCreator<java.security.PublicKey> {
-	public PublicKey createInstance(Type type) {
-		KeyPairGenerator kpg;
-		try {
-			kpg = KeyPairGenerator.getInstance("RSA");
-
-			kpg.initialize((int) Math.pow(2, 10));
-			KeyPair kp = kpg.genKeyPair();
-			return kp.getPublic();
-		} catch (NoSuchAlgorithmException e) {
-			// TODO Auto-generated catch block
-			System.out
-					.println("NoSuchAlgorithm can't happen because RSA is hardcoded");
-		}
-		return null;
-	}
-}
+//
+// class AnnotationExclusionStrategy implements ExclusionStrategy {
+//
+// @Override
+// public boolean shouldSkipField(FieldAttributes f) {
+// return f.getAnnotation(Exclude.class) != null;
+// }
+//
+// @Override
+// public boolean shouldSkipClass(Class<?> clazz) {
+// return false;
+// }
+// }
+//
+// @Retention(RetentionPolicy.RUNTIME)
+// @Target(ElementType.FIELD)
+// @interface Exclude {
+// }
